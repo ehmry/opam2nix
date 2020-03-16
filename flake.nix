7f1f78486e335ac0c551bf049dd488a6bf3954b1
Emery Hemingway <ehmry@posteo.net>
2020-03-16 14:14:54 +0530

    Remove impurities from flake evaluation

--- flake.nix
+++ flake.nix
@@ -5,12 +5,13 @@
 
   outputs = { self, nixpkgs }:
     let
-      pkg =
-        import ./default.nix { pkgs = nixpkgs.legacyPackages.x86_64-linux; };
+      pkg = nixpkgs.legacyPackages.x86_64-linux.callPackage ./nix {
+        self = self.outPath;
+      };
     in {
       packages.x86_64-linux.opam2nix = pkg;
-      defaultPackage.x86_64-linux.opam2nix =
-        self.packages.x86_64-linux.opam2nix;
+
+      defaultPackage.x86_64-linux = pkg;
 
       apps.x86_64-linux.opam2nix = {
         type = "app";
